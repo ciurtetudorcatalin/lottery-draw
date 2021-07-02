3d57943e86aa3a9b7d72e864f272d94b50836999
Catalin C <ciurtetudorcatalin@gmail.com>
2021-07-02 13:07:49 +0300

    Created application modules according to issue #5

---
+++ src/app/features/draw/draw.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DrawConfigModule } from './draw-config.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DrawConfigModule
+  ]
+})
+export class DrawModule { }
---
+++ src/app/features/history/history.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HistoryConfigModule } from './history-config.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HistoryConfigModule
+  ]
+})
+export class HistoryModule { }
