30c0f700f50a10b220dd40eccda61921bc16a5e8
Catalin C <ciurtetudorcatalin@gmail.com>
2021-07-04 17:44:06 +0300

    Complete the DrawModule using the provided diagrams #21

--- src/app/features/draw/draw.module.ts
+++ src/app/features/draw/draw.module.ts
@@ -3,16 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DrawConfigModule } from './config/draw-config.module';
 import { NumberDrawComponent } from './components/number-draw/number-draw.component';
-import { DrawComponent } from './components/draw/draw.component';
-import { DrawEndedComponent } from './components/draw-ended/draw-ended.component';
 import { PromptsModule } from '@shared/components/prompts/prompts.module';
 
 
 @NgModule({
   declarations: [
     NumberDrawComponent,
-    DrawComponent,
-    DrawEndedComponent
   ],
   imports: [
     CommonModule,
